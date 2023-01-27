3220204867fb766838289180e0bf873eb06dc970
FesalBadday <fesal.n04@hotmail.com>
2023-01-26 17:44:47 -0700

    initial commit

---
+++ assets/js/staff.mjs
@@ -0,0 +1,21 @@
+// export staff module
+export const staff = [
+  {
+    name: 'Damien Dennel',
+    bio: "Damien Dennel is a senior executive responsible for managing the financial actions of Carizzle. His duties include tracking cash flow and financial planning as well as analyzing Carizzle's financial strengths and weaknesses and proposing corrective actions.",
+    title: "CFO OF Carizzle",
+    img: "assets/images/damien-dennel.png"
+  },
+  {
+    name: 'Lucie Leclair',
+    bio: "Lucie Leclair is the highest-ranking executive in Carizzle, her primary responsibilities include making major corporate decisions, managing the overall operations and resources of Carizzle, acting as the main point of communication between the board and corporate.",
+    title: "CEO OF Carizzle",
+    img: "assets/images/lucie-leclair.png"
+  },
+  {
+    name: 'Afon Dewey',
+    bio: "Afon Dewey is the second-highest C-suite executive rank after Lucie Leclair. His primary responsibility is to oversee business operations, which may include marketing and sales, human resources, research and development, production, and other functions.",
+    title: "COO OF Carizzle",
+    img: "assets/images/afon-dewey.png"
+  }
+];
